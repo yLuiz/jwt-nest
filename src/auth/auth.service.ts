cb6fa10e45d19d123af4da8be643161a89d1f1d4
Luiz Andrade <luizvictor1231@gmail.com>
2022-06-27 13:10:49 -0400

    API Nest com JWT finalizada!

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { User } from '../user/entities/user.entity';
+import { UserService } from '../user/user.service';
+import { UserToken } from './models/UserToken';
+
+interface UserPayload {
+  sub: number
+  email: string
+  name: string
+}
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UserService,
+  ) {}
+
+  async login(user: User): Promise<UserToken> {
+    const payload: UserPayload = {
+      sub: user.id,
+      email: user.email,
+      name: user.name,
+    };
+
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  async validateUser(email: string, password: string): Promise<User> {
+    const user = await this.userService.findByEmail(email);
+
+    if (user) {
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (isPasswordValid) {
+        return {
+          ...user,
+          password: undefined,
+        };
+      }
+    }
+
+    throw new Error(
+      'Email address or password provided is incorrect.',
+    );
+  }
+}
---
+++ src/auth/models/UserPayLoad.ts
@@ -0,0 +1,5 @@
+export interface UserPayload {
+    sub: number,
+    email: string,
+    name: string
+}
